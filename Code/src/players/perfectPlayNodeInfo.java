c5cf82e584e6bc08bfabd4eef51403883663e520
raph.byrne@gmail.com <raph.byrne@gmail.com@9ce9f307-0bc1-b186-8740-293ad4a74d8b>
2011-05-28 12:58:25 +0000

    added perfect play node info

---
+++ Code/src/players/perfectPlayNodeInfo.java
@@ -0,0 +1,49 @@
+package players;
+
+import quarto.QBoard;
+import search.Node;
+import search.NodeInfo;
+
+public class perfectPlayNodeInfo implements NodeInfo {
+
+	boolean isOne;
+	double depthLim;
+	
+	public perfectPlayNodeInfo(boolean isOne, double depthLim) {
+		super();
+		this.isOne = isOne;
+		this.depthLim = depthLim;
+	}
+
+	@Override
+	public boolean isGoal(Node node) {
+		QBoard board  = (QBoard)node.getState();
+		return board.isWinningBoard() && (board.oneToMove() != isOne);
+	}
+
+	@Override
+	public boolean isTerminal(Node node) {
+		QBoard board  = (QBoard)node.getState();
+		return board.gameOver() || node.getPath().size() >= depthLim;
+	}
+
+	@Override
+	public double utility(Node node) {
+		QBoard board  = (QBoard)node.getState();
+		if(isGoal(node)) return 1;
+		else 
+			if(board.isWinningBoard()) return -1;
+			else return 0;
+	}
+
+	@Override
+	public void setDepthLimit(double limit) {
+		depthLim = limit;
+	}
+
+	@Override
+	public double getDepthLimit() {
+		return depthLim;
+	}
+
+}
